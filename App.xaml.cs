3c20164c50b57d266f8047e49608891ea6242640
FlaviusCrisan <162991075+FlaviusCrisan@users.noreply.github.com>
2025-01-02 11:40:43 +0000

    Added base files
    Simple front page with login + gives user popup if invalid entry + if valid entry the page is cleared

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace wordleProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace wordleProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            // Check if the entry has one or more letter/number
+            if (!string.IsNullOrEmpty(NameEntry.Text) && NameEntry.Text.Any(char.IsLetterOrDigit))
+            {
+                // Hide the Wordle label and login layout
+                WordleLabel.IsVisible = false;
+                LoginLayout.IsVisible = false;
+            }
+            else
+            {
+                // Display an alert if the input is invalid
+                DisplayAlert("Invalid Input", "Please enter at least one letter or number.", "OK");
+            }
+        }
+    }
+}
